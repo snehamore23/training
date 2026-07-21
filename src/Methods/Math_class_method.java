7d02b8f79698be351b265ab051e2c9868aa15efa
snehamore23 <moresneha806@gmail.com>
2026-07-21 16:52:21 +0530

    initial commit

---
+++ src/Methods/Math_class_method.java
@@ -0,0 +1,21 @@
+package Methods;
+
+public class Math_class_method {
+
+	public static void main(String[] args) {
+		int i= -9;
+		int j= 2;
+		
+		System.out.println(Math.abs(i));
+		System.out.println(Math.abs(j));
+
+		
+		
+		int n1 =10;
+		int n2 =20;
+		System.out.println(Math.max(n1,n2));
+		System.out.println(Math.min(n1,n2));
+		syso
+	}
+
+}
---
+++ src/Methods/Scanner_class_method.java
@@ -0,0 +1,30 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class Scanner_class_method {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner (System.in);
+		System.out.println("enter the int type of data");
+		int a=sc.nextInt();
+		System.out.println("the integer data is"+a);
+		
+		System.out.println("enter the decimal type of data");
+		double b=sc.nextDouble();
+		System.out.println("the decimal data is"+b);
+		
+		System.out.println("enter the boolean type of data");
+		boolean c=sc.nextBoolean();
+		System.out.println("enter boolean data is"+c);
+		
+		System.out.println("enter the String type of data");
+		String d=sc.next();
+		System.out.println("the string data is"+d);
+
+
+
+
+	}
+
+}
