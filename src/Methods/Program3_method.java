8baf197434bab0dcdbcb3972c52ed3d6a3450dfd
snehamore23 <moresneha806@gmail.com>
2026-07-24 23:14:42 +0530

    done with methods

--- src/Methods/Program3_method.java
+++ src/Methods/Program3_method.java
@@ -5,14 +5,23 @@ public class Program3_method {
 	public static void m1() {
 		int age = 18;
 
-        if (age >= 18)
+        if (age >= 18) {
             System.out.println("Eligible to Vote");
-
-        if (age < 18)
+	}
+	}
+	public static void m2() {
+        int age=16;
+        if (age < 18) {
             System.out.println("Not Eligible to Vote");
+	}
+	}
 	public static void main(String[] args) {
+		m1();
+		m2();
+		
+	}
 	
 
 	}
 
-}
+
