b188b56910482d2f98c151b99305111b479b0676
snehamore23 <moresneha806@gmail.com>
2026-07-27 12:15:09 +0530

    initial commit

--- src/HasA_relationship/BankAccount.java
+++ src/HasA_relationship/BankAccount.java
@@ -1,7 +1,39 @@
 package HasA_relationship;
 
 public class BankAccount {
+	
+	long accountNumber;
+	double balance;
+	String bankName;
+	String IFSC;
+	
+	
+	public void deposite(int amount) {
+		balance+=amount;
+		System.out.println("amount is deposited: "+amount);
+		
+	}
+	public void withdraw(int amount) {
+		balance-=amount;
+		System.out.println("amount withdrawn: " +amount);
+		
+	}
+	public void displayBalance() {
+		System.out.println("balance amount: "+balance);
+	}
 
+	public void displayAccountInformation() {
+		System.out.println("account number: "+accountNumber );
+		System.out.println("balance: "+balance);
+		System.out.println("bankName: " +bankName);
+		System.out.println("IFCS code: "+IFSC);
+	}
+	public BankAccount(long accountNumber, double balance,String bankName,String IFSC) {
+		this.accountNumber=accountNumber;
+		this.balance=balance;
+		this.bankName=bankName;
+		this.IFSC=IFSC;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- src/HasA_relationship/Customer.java
+++ src/HasA_relationship/Customer.java
@@ -2,9 +2,38 @@ package HasA_relationship;
 
 public class Customer {
 
+	String name;
+	int id;
+	long phone;
+	BankAccount account;
+	
+	public void display() {
+		System.out.println("name of customer: "+name);
+		System.out.println("id of customer: "+id);
+		System.out.println("phone number of customer is: "+phone);
+	}
+	
+	public Customer(String name, int id,long phone,BankAccount account) {
+		this.name=name;
+		this.id=id;
+		this.phone=phone;
+		this.account=account;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BankAccount acc1=new BankAccount(1234567891l, 1000.0 ,"SBI","sbi@123" );
+//		acc1.deposite(0);
+//		acc1.withdraw(0);
+//		acc1.displayAccountInformation();
+		
+			Customer c1=new Customer("sneha", 1, 2324668l, acc1);
+			c1.display();
+			System.out.println();
+			c1.account.displayAccountInformation();
+			c1.account.deposite(1000);
+			c1.account.withdraw(500);
+			c1.account.displayBalance();			
+			
+		}
 
 	}
 
-}
