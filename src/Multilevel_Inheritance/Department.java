cf70b23b2dc342b576bcdea3db2928a51fb366ee
snehamore23 <moresneha806@gmail.com>
2026-07-28 21:16:12 +0530

    initial commit

--- src/Multilevel_Inheritance/Department.java
+++ src/Multilevel_Inheritance/Department.java
@@ -1,14 +1,27 @@
 package Multilevel_Inheritance;
 
 public class Department extends College {
+	String departmentName;
+	
 
-	int departmentName;
+	public Department(String collegeName, String location,String departmentName) {
+		super(collegeName, location);
+		this.departmentName=departmentName;
+		
+		// TODO Auto-generated constructor stub
+	}
+
 	
 	public void departmentinfo() {
 		System.out.println("DepartmentName: " +departmentName);
+		
 	}
 	
 	public static void main(String[] args) {
+		Department d=new Department("PVGCOE", "Nashik", "IT");
+		d.collegeInfo();
+		d.departmentinfo();
+				
 		// TODO Auto-generated method stub
 
 	}
