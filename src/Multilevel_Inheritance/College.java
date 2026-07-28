e7426bd031e1b51036bf3848e0d0c9173fea3edc
snehamore23 <moresneha806@gmail.com>
2026-07-28 21:15:47 +0530

    initial commit

--- src/Multilevel_Inheritance/College.java
+++ src/Multilevel_Inheritance/College.java
@@ -2,20 +2,22 @@ package Multilevel_Inheritance;
 
 public class College {
 
-	int collegeName;
+	String collegeName;
     String location;
     
     public void collegeInfo() {
     	System.out.println("CollegeName: " +collegeName);
 System.out.println("Location: " +location);    
 }
-    public College(String collegeName, String location);{
+    public College(String collegeName, String location){
     this.collegeName=collegeName;
     this.location=location;
     }
     
 	public static void main(String[] args) {
-		College c= new College();
+		College c= new College("pvg","nashik");
+		c.collegeInfo();
+		
 		
 		// TODO Auto-generated method stub
 
