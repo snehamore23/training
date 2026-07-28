f17b48aa2d17cc7807dce6e1d00a665b93e8c9fc
snehamore23 <moresneha806@gmail.com>
2026-07-28 21:16:29 +0530

    initial commit

--- src/Multilevel_Inheritance/Student.java
+++ src/Multilevel_Inheritance/Student.java
@@ -1,16 +1,36 @@
 package Multilevel_Inheritance;
 
 public class Student extends Department {
-int rollno;
-String StudentName;
 
-public void studentInfo() {
-	System.out.println("Student rollno");
-	System.out.println("Student name");
-}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    int rollno;
+    String studentName;
 
-	}
+    public Student(String collegeName, String location,
+                   String departmentName, int rollno, String studentName) {
 
+        super(collegeName, location, departmentName);
+
+        this.rollno = rollno;
+        this.studentName = studentName;
+    }
+
+    public void studentInfo() {
+        System.out.println("Student Roll No: " + rollno);
+        System.out.println("Student Name: " + studentName);
+    }
+
+    public static void main(String[] args) {
+
+        Student s = new Student(
+                "PVGCOE",
+                "Nashik",
+                "IT",
+                12,
+                "Joe"
+        );
+
+        s.collegeInfo();
+        s.departmentinfo();
+        s.studentInfo();
+    }
 }
