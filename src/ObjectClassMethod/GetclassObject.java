a5010db95b7e134a3d5d214a5849301ea619ccb2
snehamore23 <moresneha806@gmail.com>
2026-08-01 16:45:15 +0530

    demo of getclass method

---
+++ src/ObjectClassMethod/GetclassObject.java
@@ -0,0 +1,15 @@
+package ObjectClassMethod;
+
+public class GetclassObject {
+
+	public static void main(String[] args) {
+		GetclassObject gc=new GetclassObject();
+		System.out.println(gc.getClass());
+		System.out.println(gc.getClass().getName());
+		System.out.println(gc.getClass().getPackage());
+		System.out.println(gc.getClass().getPackageName());
+		// TODO Auto-generated method stub
+
+	}
+
+}
