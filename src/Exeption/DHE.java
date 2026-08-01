bf470ee495b1783c7623a73318b6656a08bc38d4
snehamore23 <moresneha806@gmail.com>
2026-08-01 10:56:45 +0530

    exception

---
+++ src/Exeption/DHE.java
@@ -0,0 +1,21 @@
+package Exeption;
+
+public class DHE {
+
+	public static void main(String[] args) {
+		System.out.println("statement 0");
+		System.out.println("statement 1");
+		
+		try {
+			throw new ArithmeticException();
+		}
+		catch (ArithmeticException e) {
+			e.printStackTrace();
+		}
+		 System.out.println("statement 2");
+		 System.out.println("statement 3");
+		// TODO Auto-generated method stub
+
+	}
+
+}
