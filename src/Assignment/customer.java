f7c2adb51eb51a0f401e1a3446e3afda08013b35
snehamore23 <moresneha806@gmail.com>
2026-08-03 22:37:57 +0530

    initial commit

---
+++ src/Assignment/customer.java
@@ -0,0 +1,27 @@
+package Assignment;
+
+public class customer {
+	
+	String name;
+	int customerid;
+	String address;
+
+	public customer(String name, int i, String string) {
+		super();
+		this.name = name;
+		this.customerid = i;
+		this.address = string;
+	}
+
+	public void displaycustomerinfo() {
+		System.out.println("Customer name: "+name);
+		System.out.println("CustomerId: "+customerid);
+		System.out.println("Address: "+address);
+	}
+	public static void main(String[] args) {
+		customer c=new customer("joe",2,"Nashik");
+		// TODO Auto-generated method stub
+
+	}
+
+}
