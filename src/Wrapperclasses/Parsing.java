7d23ea8ae3f87aae61e6dc87a9cfd32bbd819ebd
snehamore23 <moresneha806@gmail.com>
2026-08-03 15:22:23 +0530

    done with parsing

---
+++ src/Wrapperclasses/Parsing.java
@@ -0,0 +1,23 @@
+package Wrapperclasses;
+
+public class Parsing {
+	
+
+	public static void main(String[] args) {
+		String a="12";
+		int value1 =Integer.parseInt(a);
+		System.out.println(value1);
+		
+		String b="20.0";
+         double value2=Double.parseDouble(b);
+         System.out.println(value2);
+         
+  
+         String d="true";
+         boolean value4=Boolean.parseBoolean(d);
+         System.out.println(value4);
+	
+		// TODO Auto-generated method st
+	}
+
+}
