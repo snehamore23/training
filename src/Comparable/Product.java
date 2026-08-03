dae00cda4184779faba63b2c230e7be10cf50955
snehamore23 <moresneha806@gmail.com>
2026-08-03 15:23:21 +0530

    done with product program

---
+++ src/Comparable/Product.java
@@ -0,0 +1,14 @@
+package Comparable;
+
+public class Product {
+int pid;
+String pname;
+int quntity;
+double price;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
