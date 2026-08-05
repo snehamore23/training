c5e05da140e292d1a097e189a425862961139f89
snehamore23 <moresneha806@gmail.com>
2026-08-05 22:18:43 +0530

    initial commit

---
+++ src/Collection/LinkedhashsetExx.java
@@ -0,0 +1,35 @@
+package Collection;
+
+import java.util.LinkedHashSet;
+
+
+public class LinkedhashsetExx {
+
+	public static void main(String[] args) {
+		LinkedHashSet l=new LinkedHashSet();
+		l.add(10);
+		l.add(20);
+		l.add(30);
+		l.add(50);
+		l.add(40);
+		System.out.println(l);
+		
+//		System.out.println(l.remove(40));
+//		System.out.println(l.contains(20));
+//		System.out.println(l.size());
+//		//System.out.println(h.clean());
+//		System.out.println(l.iterator());
+//		System.out.println(l.addAll(l));
+////		System.out.println(h.removeAll(h);
+////		System.out.println(h.retainAll(h));
+//         h.containsAll(l);
+//                  System.out.println(l);
+//         h.clone()
+//                 System.out.println(l);
+//
+//		System.out.println(l);
+		// TODO Auto-generated method stub
+
+	}
+
+}
