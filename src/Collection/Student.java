8039e2738dbda5818f30d9de7f1e77f1c312376c
snehamore23 <moresneha806@gmail.com>
2026-08-05 09:48:08 +0530

    collection framework

---
+++ src/Collection/Student.java
@@ -0,0 +1,54 @@
+package Collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Student implements Comparable<Student>{
+	
+	int age;
+	String name;
+	double marks;
+	int weight;
+	
+
+	public Student(int age, String name, double marks, int weight) {
+		super();
+		this.age = age;
+		this.name = name;
+		this.marks = marks;
+		this.weight = weight;
+	}
+	@Override
+	public int compareTo(Student o) {
+		//return this.age-o.age;
+		//return (int) ((int)this.marks-o.marks);
+		//return this.weight-o.weight;
+		return this.name.compareTo(name);
+	}
+
+
+	@Override
+	public String toString() {
+		return "Student [age=" + age + ", name=" + name + ", marks=" + marks + ", weight=" + weight + "]";
+	}
+	public static void main(String[] args) {
+		Student s1=new Student(19,"joe",88.8,60);
+		Student s2=new Student(20,"tom",98.8,50);
+		Student s3=new Student(18,"sam",78.8,45);
+		Student s4=new Student(19,"joy",82.3,50);
+		Student s5=new Student(21,"anni",83.5,40);
+		
+		ArrayList al= new ArrayList();
+		al.add(s1);
+		al.add(s2);
+		al.add(s3);
+		al.add(s4);
+		al.add(s5);
+	
+		System.out.println(al);
+		Collections.sort(al);
+		System.out.println(al);
+		
+	}
+
+}
