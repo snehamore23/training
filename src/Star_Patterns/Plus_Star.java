de6bc7fe781f64f5ae886cf920c0a5cbbf3ec843
snehamore23 <moresneha806@gmail.com>
2026-08-07 09:19:09 +0530

    done with star pattern program

--- src/Star_Patterns/Plus_Star.java
+++ src/Star_Patterns/Plus_Star.java
@@ -4,7 +4,7 @@ public class Plus_Star {
 
 	public static void main(String[] args) {
 		for(int i=1; i<=5; i++) {
-			for(int j=1; j<=5; j++) {
+			for(int j=1; j<=2; j++) {
 				System.out.print("*");
 			}
 		System.out.println("*");
