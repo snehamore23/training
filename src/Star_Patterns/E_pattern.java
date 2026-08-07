8623d118610f766e1947d17be4ecbea485334783
snehamore23 <moresneha806@gmail.com>
2026-08-07 12:05:21 +0530

    done with E

---
+++ src/Star_Patterns/E_pattern.java
@@ -0,0 +1,19 @@
+package Star_Patterns;
+
+public class E_pattern {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			
+			for(int j=1;j<=5;j++) {
+				if(i==5||i==1||j==1||i==3)
+				System.out.print(" * ");
+				else
+					System.out.print("   ");
+				
+			}
+			System.out.println();
+
+}
+	}
+}
