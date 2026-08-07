b4a868c248ddf55351da0a376007d390fb15a352
snehamore23 <moresneha806@gmail.com>
2026-08-07 13:00:01 +0530

    done with W

---
+++ src/Star_Patterns/W_pattern.java
@@ -0,0 +1,21 @@
+package Star_Patterns;
+
+public class W_pattern {
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			
+			for(int j=1;j<=5;j++) {
+				if(j==1||j==5||i>=3&&i+j==6||i+j==8&&i==j)
+				System.out.print(" * ");
+				else
+					System.out.print("   ");
+				
+			}
+			System.out.println();
+
+}
+	}
+
+}
+
+
