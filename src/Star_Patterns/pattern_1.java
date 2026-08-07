d20f45567a30575cdc5f32addbec89cb3ae53589
snehamore23 <moresneha806@gmail.com>
2026-08-07 10:20:11 +0530

    done with pattern 1

---
+++ src/Star_Patterns/pattern_1.java
@@ -0,0 +1,20 @@
+package Star_Patterns;
+
+public class pattern_1 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			
+			for(int j=1;j<=5;j++) {
+				if(i==j|| (i+j)==6||i==1||j==5||i==5||j==1)
+				System.out.print(" * ");
+				else
+					System.out.print("   ");
+				
+			}
+			System.out.println();
+		}
+
+	}
+
+}
