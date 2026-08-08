4f434a4fe64deaf75673da2ee7c726806bfe4039
snehamore23 <moresneha806@gmail.com>
2026-08-08 23:13:10 +0530

    done with butterfly pattern

---
+++ src/Star_Patterns/Butterfly5.java
@@ -0,0 +1,29 @@
+package Star_Patterns;
+
+public class Butterfly5 {
+	public static void main(String[] args) {
+	int star=1;
+	int space=7;
+	for(int i=1;i<=9;i++) {
+		for(int j=1;j<=star;j++) {
+			System.out.print(" * ");
+		}
+		for(int j=1;j<=space;j++) {
+		System.out.print("   ");
+		}
+		for(int j=1;j<=star;j++) {	
+			if(j!=5)
+			System.out.print(" * ");
+			}
+		System.out.println();
+	if(i<=4) {
+		star++;
+	space-=2;
+	}
+	else {
+		star--;
+		space+=2;
+}
+}
+	}
+}
