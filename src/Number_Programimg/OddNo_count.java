0fad8b37d2e21711c7b2a5ea01ba57773f619051
snehamore23 <moresneha806@gmail.com>
2026-08-08 23:14:24 +0530

    doe with odd no

---
+++ src/Number_Programimg/OddNo_count.java
@@ -0,0 +1,18 @@
+package Number_Programimg;
+
+public class OddNo_count {
+
+	public static void main(String[] args) {
+          int n=12354;
+		
+		int sum=0;
+		while(n!=0) {
+			if(n%2!=0) {
+			sum=sum+n%10;
+			}
+			n=n/10;
+		}
+		System.out.println(sum);
+	}
+
+}
