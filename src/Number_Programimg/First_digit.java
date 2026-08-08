537052b324fd8e27db116e9232f20fccb94f635c
snehamore23 <moresneha806@gmail.com>
2026-08-08 23:16:11 +0530

    done with firts digit

---
+++ src/Number_Programimg/First_digit.java
@@ -0,0 +1,14 @@
+package Number_Programimg;
+
+public class First_digit {
+
+	public static void main(String[] args) {
+		int n=123467;
+		
+		while (n>9){
+			n=n/10;
+		}
+			System.out.println(n);
+		}
+	}
+
