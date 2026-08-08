be940c6f9079aaf659b0ec7f925f7d2e5b97305d
snehamore23 <moresneha806@gmail.com>
2026-08-08 23:13:23 +0530

    done with char pattern

---
+++ src/Star_Patterns/Char_pattern1.java
@@ -0,0 +1,14 @@
+package Star_Patterns;
+
+public class Char_pattern1 {
+
+	public static void main(String[] args) {
+		char ch='A';
+	for(int i=1;i<=4;i++) {
+	for(int j=1;j<=4;j++) {
+		System.out.print(ch++);
+	}
+	System.out.println();
+	}
+	}
+}
