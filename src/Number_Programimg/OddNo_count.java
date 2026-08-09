a5e0af56a028753228b468f9629123f0ac401b58
snehamore23 <moresneha806@gmail.com>
2026-08-09 22:32:59 +0530

    initial commit

--- src/Number_Programimg/OddNo_count.java
+++ src/Number_Programimg/OddNo_count.java
@@ -3,7 +3,7 @@ package Number_Programimg;
 public class OddNo_count {
 
 	public static void main(String[] args) {
-          int n=12354;
+          int n=12354567;
 		
 		int sum=0;
 		while(n!=0) {
