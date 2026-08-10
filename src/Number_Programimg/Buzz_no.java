8f351e122876c503ea2ce038f4b3a8b744802f76
snehamore23 <moresneha806@gmail.com>
2026-08-10 22:39:07 +0530

    done with buzz no

---
+++ src/Number_Programimg/Buzz_no.java
@@ -0,0 +1,16 @@
+package Number_Programimg;
+
+public class Buzz_no {
+
+	public static void main(String[] args) {
+		int n=27;
+			 if(n%7==0 || n%10==7) {
+				 System.out.println("Buzz no");
+			 }
+			 else {
+				 System.out.println("not buzz");
+		}
+
+}
+	}
+
