bd4d08b5be019820a63f9d6c6f73892b2205cb5e
snehamore23 <moresneha806@gmail.com>
2026-08-10 12:00:33 +0530

    done with sum program

---
+++ src/Number_Programimg/SumOfEven.java
@@ -0,0 +1,19 @@
+package Number_Programimg;
+
+public class SumOfEven {
+
+	public static void main(String[] args) {
+		 int n=1234;
+			
+			int sum=0;
+			while(n!=0) {
+				if(n%2==0) {
+				sum=sum+n%10;
+				}
+				n=n/10;
+			}
+			System.out.println(sum);
+
+	}
+
+}
