d3be1f1a45832f7cb546b63eb78ba787925082e8
snehamore23 <moresneha806@gmail.com>
2026-08-10 22:40:12 +0530

    done with fqactorial program

---
+++ src/Number_Programimg/Factorial_program.java
@@ -0,0 +1,16 @@
+package Number_Programimg;
+
+public class Factorial_program {
+
+
+	public static void main(String[] args) {
+		int n1=5;
+		int factorial=1;
+		for(int i=2;i<n1;i++) {
+			factorial*=i;
+
+		}
+		System.out.println(factorial);
+	}
+
+}
