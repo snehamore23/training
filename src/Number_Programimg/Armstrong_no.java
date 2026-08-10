35badd5ac02ff91f7d1a02f463e06be375f78a34
snehamore23 <moresneha806@gmail.com>
2026-08-10 22:37:27 +0530

    done with armstrong

--- src/Number_Programimg/Armstrong_no.java
+++ src/Number_Programimg/Armstrong_no.java
@@ -2,26 +2,42 @@ package Number_Programimg;
 
 public class Armstrong_no {
 
-	public static void main(String[] args) {
-		int n = 153;
+    public static int power(int n, int power) {
+        int res = 1;
+
+        for (int i = 1; i <= power; i++) {
+            res *= n;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 153;
         int original = n;
+        int temp = n;
         int sum = 0;
+        int countofdig = 0;
 
+        // Count number of digits
+        while (temp != 0) {
+            countofdig++;
+            temp = temp / 10;
+        }
+
+        // Calculate Armstrong sum
         while (n != 0) {
-
             int rem = n % 10;
-            
-            sum = sum + rem * rem * rem;
-            n=n/10;
+            sum = sum + power(rem, countofdig);
+            n = n / 10;
         }
 
-        if (sum == original) {
-            System.out.println(original + " is Armstrong Number");
+        // Check Armstrong number
+        if (original == sum) {
+            System.out.println("Armstrong Number");
         } else {
-            System.out.println(original + " is not Armstrong Number");
+            System.out.println("Not Armstrong Number");
         }
-			
-
-	}
-
+    }
 }
