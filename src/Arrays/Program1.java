531440d2b783a7913fa6373583026a9ef3479e53
snehamore23 <moresneha806@gmail.com>
2026-08-11 23:32:04 +0530

    initial commit

---
+++ src/Arrays/Program1.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+	int a[]=new int[5];
+//	System.out.println("Array address"+a);
+//	for(int i=0;i<a.length;i++) {
+//	System.out.println(a[i]);
+//
+//	}
+	//1,2,3,4,5
+	int b[]= {1,2,3,4,5};
+	System.out.println("Address of b array" +b);
+	for(int i=1;i<=b.length;i++) {
+		System.out.println(b[i]);
+	}
+
+
+	}
+}
