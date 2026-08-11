42aa035d95c91f9204539434bdc3f0ce4d9e9a26
snehamore23 <moresneha806@gmail.com>
2026-08-11 23:28:17 +0530

    done wirh desecnding order

---
+++ src/Arrays/Descending_order.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class Descending_order {
+
+	public static void main(String[] args) {
+		int a[]= {1,2,8,6,0,10,12};
+		for(int i=0;i<a.length;i++) {
+			for(int j=i+1;j<a.length;j++) {
+				
+				if(a[i]<a[j]) {
+				int temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+				}
+			}
+		}
+		System.out.println("Descending Order: ");
+		for(int i=0;i<a.length;i++) 
+		System.out.print(a[i]+ " ");
+	}
+
+}
