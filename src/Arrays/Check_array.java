27621d2b9f6d590dc519b1d422f480914ed3577d
snehamore23 <moresneha806@gmail.com>
2026-08-11 23:28:03 +0530

    done with Check array

---
+++ src/Arrays/Check_array.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class Check_array {
+
+	public static void main(String[] args) {
+		int a[]= {1,2,8,6,0,10,12};
+		int n=0;
+		boolean found =false;
+		
+		for(int i=0;i<a.length;i++) {
+			if(a[i]==n) {
+				found=true;
+				break;
+			}
+		}
+			if(found)
+				System.out.println("Element is present");
+			else 
+				System.out.println("Element is not present");
+			}
+}
