8a92878e536b4479e64abbe5b1d572d4e5b4b033
snehamore23 <moresneha806@gmail.com>
2026-08-13 18:45:30 +0530

    done with anagram program

---
+++ src/String/Anagram.java
@@ -0,0 +1,23 @@
+package String;
+import java.util.Arrays;
+
+public class Anagram {
+
+	public static void main(String[] args) {
+		String s1="Silent".toLowerCase();
+		String s2="Listen".toLowerCase();
+		
+			char ch1[]=s1.toCharArray();
+			char ch2[]=s2.toCharArray();
+			Arrays.sort(ch1);
+			Arrays.sort(ch2);
+			
+			if(Arrays.equals (ch1,ch2))
+				System.out.println("Anagram");
+			else
+				System.out.println("Not an anagram");
+			
+		}
+
+	}
+
