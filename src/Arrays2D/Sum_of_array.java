c6e279d069c3a71538e84655eebaef477da2007b
snehamore23 <moresneha806@gmail.com>
2026-08-13 18:51:31 +0530

    done with sum of array

---
+++ src/Arrays2D/Sum_of_array.java
@@ -0,0 +1,21 @@
+package Arrays2D;
+
+public class Sum_of_array {
+
+	public static void main(String[] args) {
+		int b[][]=new int[3][3];
+		int a[][]= {{1,2,3},{4,5,6},{7,8,9}};
+		int sum=0;
+		for(int row=0;row<a.length;row++) {
+			for(int col=0;col<a[0].length;col++) {
+				System.out.print(a[row][col]+ " ");
+			
+			}
+		}
+			System.out.println();
+		}
+
+
+	}
+
+
