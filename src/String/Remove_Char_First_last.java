57e9f7024a7e8c99c09fe97f97f32eb390cb23ca
snehamore23 <moresneha806@gmail.com>
2026-08-13 18:48:21 +0530

    remove char first and last

---
+++ src/String/Remove_Char_First_last.java
@@ -0,0 +1,11 @@
+package String;
+
+public class Remove_Char_First_last {
+
+	public static void main(String[] args) {
+		String s= "rahul agraval";
+		s=s.substring(1, s.length()-1);
+		System.out.println(s);
+		}
+
+	}
